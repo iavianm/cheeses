7c7b469ce4d493b4b10c7d70eedac3af7c5079ab
Mihail Iavian <85059359+iavianm@users.noreply.github.com>
2024-04-05 23:37:50 +0300

    Feature/css (#1)

---
+++ components/Header/Header.jsx
@@ -0,0 +1,12 @@
+"use client";
+import styles from "./Header.module.css";
+
+function Header() {
+  return (
+    <header className={styles.header}>
+      <h1>Hello</h1>
+    </header>
+  );
+}
+
+export default Header;
