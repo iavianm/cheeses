e7678034c17806dd0e5f171602b1bedce1120386
iavianm <iavianm@mail.ru>
2024-04-06 01:17:13 +0300

    add logo

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -1,10 +1,13 @@
 "use client";
 import styles from "./Header.module.css";
+import Link from "next/link";
 
 function Header() {
   return (
     <header className={styles.header}>
-      <h1>Hello</h1>
+      <div className={styles.header__body}>
+        <Link href="/" className={styles.header__logo} />
+      </div>
     </header>
   );
 }
