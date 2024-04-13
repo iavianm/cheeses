a4c5ee5d61c2da70cf285608da5ed58ed78cd8af
Mihail Iavian <85059359+iavianm@users.noreply.github.com>
2024-04-13 14:05:27 +0300

    Feature/header (#2)

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -1,10 +1,15 @@
-"use client";
 import styles from "./Header.module.css";
+import Link from "next/link";
+import Navigation from "@/components/Navigation/Navigation";
+import { navItems } from "../../content/navItemsContent.json";
 
 function Header() {
   return (
     <header className={styles.header}>
-      <h1>Hello</h1>
+      <Link href="/" className={styles.header__logo} />
+      <div className={styles.header__body}>
+        <Navigation navLinks={navItems} />
+      </div>
     </header>
   );
 }
