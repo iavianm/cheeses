1b55c1a9ca9f2efd414c3697fca77cb0a03a5390
iavianm <iavianm@mail.ru>
2024-04-18 19:18:05 +0300

    add about block

---
+++ components/Footer/Footer.jsx
@@ -0,0 +1,52 @@
+import styles from "./Footer.module.css";
+import Link from "next/link";
+import { navItems } from "../../content/navItemsContent.json";
+
+const Footer = () => {
+  const currentYear = new Date().getFullYear();
+
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.footer__content}>
+        <div className={styles.footer__section}>
+          <Link href="/" className={styles.footer__link}>
+            <img
+              src={"/images/logoY.png"}
+              alt={"logo"}
+              className={styles.footer__logo}
+            />
+          </Link>
+          <ul className={styles.footer__nav}>
+            {navItems.map((link) => {
+              return (
+                <li key={link.label}>
+                  <Link href={link.href}>{link.label}</Link>
+                </li>
+              );
+            })}
+          </ul>
+
+          <address className={styles.footer__address}>
+            <div>
+              <p className={styles.footer__address_text}>
+                Тульская область, Ясногорский район,
+              </p>
+              <p className={styles.footer__address_text}>д. Санталово</p>
+            </div>
+
+            <div className={styles.footer__address_links}>
+              <a href="tel:+79208888888">+7 920 888 88 88</a>
+
+              <a href="mailto:somesite@yandex.ru">somesite@yandex.ru</a>
+            </div>
+          </address>
+        </div>
+        <div className={`${styles.footer__section} ${styles.footer__copy}`}>
+          <p>&copy;{currentYear} Ясногорские сыры. Все права защищены</p>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ components/PartnersBlock/PartnersBlock.jsx
@@ -0,0 +1,33 @@
+import styles from "@/components/PartnersBlock/PartnersBlock.module.css";
+import Link from "next/link";
+import SliderPartnersBlock from "@/components/SliderPartnersBlock/SliderPartnersBlock";
+import { partnersImages } from "../../content/sliderPartnersImages.json";
+
+function PartnersBlock() {
+  return (
+    <section className={styles.partners__section}>
+      <div className={styles.partners__container}>
+        <div className={styles.partners__head}>
+          <h1 className={styles.partners__head_title}>Партнеры</h1>
+          <Link className={styles.partners__head_link} href={"/"}>
+            смотреть все
+          </Link>
+        </div>
+        <article className={styles.partners__article}>
+          Наши продукты, созданные с учетом передовых разработок, полностью
+          соответствуют отечественным нормам и международным стандартам
+          качества. Это стало возможным благодаря тесному взаимодействию с
+          ведущими предприятиями общественного питания и крупнейшими розничными
+          сетями, что позволяет нам стабильно проходить независимый аудит и
+          укреплять партнерские отношения с ключевыми дистрибьюторами в
+          различных регионах.
+        </article>
+        <div className={styles.partners__slider}>
+          <SliderPartnersBlock content={partnersImages} />
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default PartnersBlock;
