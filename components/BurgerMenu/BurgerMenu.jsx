f4e36ef567fa50029bef48d9cea7fd607efa8aaa
Mihail Iavian <85059359+iavianm@users.noreply.github.com>
2024-04-26 15:09:38 +0300

    Feature/burger_menu (#8)

--- components/BurgerMenu/BurgerMenu.jsx
+++ components/BurgerMenu/BurgerMenu.jsx
@@ -1,7 +1,7 @@
 "use client";
 import styles from "./BurgerMenu.module.css";
 import Link from "next/link";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { usePathname } from "next/navigation";
 import { ReactSVG } from "react-svg";
 
@@ -13,77 +13,79 @@ function BurgerMenu(props) {
     setBurgerOpened(!burgerOpened);
   }
 
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth > 1024) {
+        setBurgerOpened(false);
+      }
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <>
       <button className={styles.header_burger__btn} onClick={handleOpenBurger}>
         <img
           className={styles.header_burger__img}
           src="/images/burger-icon.svg"
-          alt="кнопка меню"
+          alt="menu_button"
         ></img>
       </button>
-      {burgerOpened ? (
-        <div className={styles.burger__overlay}>
-          <div className={styles.burger__menu}>
-            <button className={styles.burger__close} onClick={handleOpenBurger}>
-              <img
-                src="/images/close-icon.svg"
-                alt="крестик"
-                className={styles.image__close}
-              ></img>
-            </button>
-            <nav className={styles.burger__nav}>
-              <ul className={styles.burger__list}>
-                {props.navLinks.map((link) => {
-                  const isActive = pathname === link.href;
 
-                  return (
-                    <li key={link.label}>
-                      <Link
-                        href={link.href}
-                        className={
-                          isActive
-                            ? `${styles.active} ${styles.nav__menu_item}`
-                            : styles.nav__menu_item
-                        }
-                      >
-                        <ReactSVG
-                          src={link.src}
-                          alt={link.label}
-                          className={styles.nav__icon}
-                        />
-                        {link.label}
-                      </Link>
-                    </li>
-                  );
-                })}
-                {/*<li>*/}
-                {/*  <Link href="/" className={styles.burger__item}>*/}
-                {/*    Продукция*/}
-                {/*  </Link>*/}
-                {/*</li>*/}
-                {/*<li>*/}
-                {/*  <Link href="/" className={styles.burger__item}>*/}
-                {/*    Контакты*/}
-                {/*  </Link>*/}
-                {/*</li>*/}
-                {/*<li>*/}
-                {/*  <Link href="/" className={styles.burger__item}>*/}
-                {/*    О нас*/}
-                {/*  </Link>*/}
-                {/*</li>*/}
-                {/*<li>*/}
-                {/*  <Link href="/" className={styles.burger__item}>*/}
-                {/*    Партнеры*/}
-                {/*  </Link>*/}
-                {/*</li>*/}
-              </ul>
-            </nav>
-          </div>
+      <div
+        className={`${styles.burger__overlay} ${
+          burgerOpened ? styles.burger__overlay_visible : ""
+        }`}
+        onClick={() => setBurgerOpened(false)}
+      >
+        <div
+          className={`${styles.burger__menu} ${
+            burgerOpened ? styles.burger__menu_visible : ""
+          }`}
+          onClick={(e) => e.stopPropagation()}
+        >
+          <button className={styles.burger__close} onClick={handleOpenBurger}>
+            <img
+              src="/images/close-icon.svg"
+              alt="close_button"
+              className={styles.image__close}
+            ></img>
+          </button>
+          <nav className={styles.burger__nav}>
+            <ul className={styles.burger__list}>
+              {props.navLinks.map((link) => {
+                const isActive = pathname === link.href;
+
+                return (
+                  <li key={link.label}>
+                    <Link
+                      href={link.href}
+                      className={
+                        isActive
+                          ? `${styles.active} ${styles.nav__menu_item}`
+                          : styles.nav__menu_item
+                      }
+                      onClick={handleOpenBurger}
+                    >
+                      <ReactSVG
+                        src={link.src}
+                        alt={link.label}
+                        className={styles.nav__icon}
+                      />
+                      {link.label}
+                    </Link>
+                  </li>
+                );
+              })}
+            </ul>
+          </nav>
         </div>
-      ) : (
-        false
-      )}
+      </div>
     </>
   );
 }
